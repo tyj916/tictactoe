df5684bafe439bc89c89bf608e2c2dedef76b457
Tan Yi Jun <tanyijun6@gmail.com>
2024-01-19 19:01:42 +0800

    Create game object

--- script.js
+++ script.js
@@ -27,5 +27,32 @@ function createPlayer(name, mark) {
   }
 }
 
+function startGame(p1 = "Player 1", p2 = "Player 2") {
+  const gameboard = createGameboard();
+
+  const player1 = createPlayer(p1, "X");
+  const player2 = createPlayer(p2, "O");
+  const players = [player1, player2];
+  let currentPlayer = players[0].getMark() === "X" ? players[0] : players[1];
+
+  const getCurrentPlayer = () => currentPlayer.getName();
+
+  function switchCurrentPlayer() {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[2];
+  }
+
+  function playRound() {
+    
+  }
+
+  return {
+    getCurrentPlayer,
+    switchCurrentPlayer,
+    playRound,
+  }
+}
+
+
 const gb = createGameboard();
-const player1 = createPlayer("Player 1", "X");
+const player = createPlayer("Player 1", "X");
+const game = startGame();
