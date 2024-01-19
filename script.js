435655bdcee5e2ae11cf7a01de0631a618761feb
Tan Yi Jun <tanyijun6@gmail.com>
2024-01-19 18:09:59 +0800

    Create gameboard

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function createGameboard(row = 3, col = 3) {
+  const gameboard = [];
+
+  for (let i = 0; i < row; i++) {
+    gameboard.push([]);
+    for(let j = 0; j < col; j++) {
+      gameboard[i].push("");
+    }
+  }
+
+  const getGameboard = () => gameboard;
+
+  return {
+    getGameboard,
+  }
+}
+
+const gb = createGameboard();
